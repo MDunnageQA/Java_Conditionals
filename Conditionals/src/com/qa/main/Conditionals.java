ad3a4fb902cd67943b091d11fc07a324f4e704f2
MDunnageQA <62284099+MDunnageQA@users.noreply.github.com>
2020-03-24 08:49:45 +0000

    step 3
    Modify your method from conditionals 1 to have another if statement that checks if one of the both the numbers are even. If this is true, return 0.

--- Conditionals/src/com/qa/main/Conditionals.java
+++ Conditionals/src/com/qa/main/Conditionals.java
@@ -2,7 +2,10 @@ package com.qa.main;
 
 public class Conditionals {
 	public int conditionalsInput(int numberOne, int numberTwo, boolean inputBool) {
-
+		if (numberOne % 2 == 0 || numberTwo % 2 == 0) {
+			System.out.println("Whoops! Even number");
+			return 0;
+		}
 		
 		if (inputBool == true) {
 			return numberOne + numberTwo;
