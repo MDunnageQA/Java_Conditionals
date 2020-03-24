79ed259dcb2a578c0b6a7a3e07acbcd236671eda
MDunnageQA <62284099+MDunnageQA@users.noreply.github.com>
2020-03-24 08:48:20 +0000

    Create a method which accepts 3 parameters, 2 integers and a Boolean,

---
+++ Conditionals/src/com/qa/main/Conditionals.java
@@ -0,0 +1,15 @@
+package com.qa.main;
+
+public class Conditionals {
+	public int conditionalsInput(int numberOne, int numberTwo, boolean inputBool) {
+
+		
+		if (inputBool == true) {
+			return numberOne + numberTwo;
+		}
+		else {
+			return numberOne * numberTwo;
+		}
+	}
+
+}
---
+++ Conditionals/src/com/qa/main/Runner.java
@@ -0,0 +1,11 @@
+package com.qa.main;
+
+public class Runner {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Conditionals condition = new Conditionals();
+		System.out.println(condition.conditionalsInput(5, 7, false));
+	}
+
+}
